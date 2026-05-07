85526e48f6a15e750b7c6c0b985bdba840c72c2e
G. <rmgolamrabbani@gmail.com>
2026-05-07 14:42:08 +0600

    fix file path

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -21,12 +21,12 @@ const Footer = () => {
   const socials = [
     {
       name: "GitHub",
-      href: "#",
+      href: "https://github.com/rmgolamrabbani",
       path: "M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"
     },
     {
       name: "LinkedIn",
-      href: "#",
+      href: "https://www.linkedin.com/in/rmgolamrabbani01/",
       path: "M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6zM2 9h4v12H2z"
     },
     {
@@ -58,6 +58,8 @@ const Footer = () => {
                 <Magnetic key={social.name}>
                   <a
                     href={social.href}
+                    target="_blank"
+                    rel="noopener noreferrer"
                     className="w-12 h-12 glass-panel rounded-xl flex items-center justify-center text-on-surface-variant hover:text-primary hover:border-primary/50 transition-all shadow-xl"
                     aria-label={social.name}
                   >
